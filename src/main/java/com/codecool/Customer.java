84538cc4ea82fd8335f0c2da28a7044c9c280d30
polbox93 <polbox93@gmail.com>
2017-12-03 14:19:00 +0100

    Implement full classes: Customer and Sale

--- src/main/java/com/codecool/Customer.java
+++ src/main/java/com/codecool/Customer.java
@@ -1,4 +1,81 @@
 package com.codecool;
 
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    private String name;
+    private String surname;
+    private int birthyear;
+    private String gender;
+    private List<Sale> sales = new ArrayList<>();
+
+
+    public void Customer() {
+
+    }
+
+    public void Customer(String name, String surname, int birthyear, String gender) {
+        this.name = name;
+        this.surname = surname;
+        this.birthyear = birthyear;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getBirthyear() {
+        return birthyear;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public int getAge() {
+        int actualYear = Year.now().getValue();
+        int age = actualYear - getBirthyear();
+        return age;
+    }
+
+    public void setName(String name) {
+        if(name.equals("")){
+            throw new IllegalArgumentException("Name name can't be empty");
+        }
+        if(name.equals(null)) {
+            throw new NullPointerException("Name name can't be empty");
+        }
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        if(surname.equals("")){
+            throw new IllegalArgumentException("Surname name can't be empty");
+        }
+        if(surname.equals(null)) {
+            throw new NullPointerException("Surname name can't be empty");
+        }
+        this.surname = surname;
+    }
+
+    public void setBirthyear(int birthyear) {
+        if(!(birthyear > 1900 && birthyear < Year.now().getValue())) {
+            throw new IllegalArgumentException("Birthyear must be bigger than 1900");
+        }
+        this.birthyear = birthyear;
+    }
+
+    public void setGender(String gender) {
+        if(!(gender.equals("male") || gender.equals("female"))) {
+            throw new IllegalArgumentException("Gender must be male or female");
+        }
+        this.gender = gender;
+    }
 }
--- src/main/java/com/codecool/Sale.java
+++ src/main/java/com/codecool/Sale.java
@@ -19,4 +19,46 @@ public class Sale {
     }
 
 
+    public void setProductName(String productName) {
+        if(productName.equals("")){
+            throw new IllegalArgumentException("Product name can't be empty");
+        }
+        if(productName.equals(null)) {
+            throw new NullPointerException("Product name can't be empty");
+        }
+        this.productName = productName;
+    }
+
+    public void setNetValue(BigDecimal netValue) {
+        if(netValue.signum() <= 0 || netValue.scale() > 2) {
+            throw new IllegalArgumentException("Net value must be a positive number with at most 2 decimal point places");
+        }
+        this.netValue = netValue;
+    }
+
+    public void setTaxRate(int taxRate) {
+        if(!(taxRate > 0 && taxRate < 100)){
+            throw new IllegalArgumentException("Taxrate must be between 0 and 100");
+        }
+        this.taxRate = taxRate;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public BigDecimal getNetValue() {
+        return netValue;
+    }
+
+    public int getTaxRate() {
+        return taxRate;
+    }
+
+
+    public BigDecimal getGrossValue() {
+        BigDecimal taxValue = this.getNetValue().multiply(new BigDecimal(0.01)) ;
+
+        return this.getNetValue().add(taxValue);
+    }
 }
